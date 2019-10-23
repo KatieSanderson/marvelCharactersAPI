44a41af8ab0cd8313affe7c4adeb95b4873c34ca
Katie Sanderson <katherina.sanderson@gmail.com>
2019-10-23 02:43:29 +0100

    Added Translator class

--- src/main/java/katie/marvel/util/Translator.java
+++ src/main/java/katie/marvel/util/Translator.java
@@ -1,4 +1,22 @@
 package katie.marvel.util;
 
+import co.aurasphere.jyandex.Jyandex;
+import co.aurasphere.jyandex.dto.Language;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Arrays;
+
+@PropertySource("file:application.properties")
 public class Translator {
+
+    @Value("${translate.key}")
+    private static String apiKey;
+
+    public static String translate(String textToTranslate, String desiredLanguage) {
+        Jyandex client = new Jyandex(apiKey);
+        System.out.println(textToTranslate + " " + desiredLanguage);
+        System.out.println(Arrays.toString(client.translateText(textToTranslate, Language.ENGLISH, desiredLanguage).getTranslatedText()));
+        return "";
+    }
 }

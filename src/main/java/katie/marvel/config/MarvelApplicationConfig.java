f23671c7c67a2988379d66f9ef6cb1871e2047e3
Katie Sanderson <katherina.sanderson@gmail.com>
2019-10-23 21:16:28 +0100

    Changed jyandex client to Translator constructor Changed config file

--- src/main/java/katie/marvel/config/MarvelApplicationConfig.java
+++ src/main/java/katie/marvel/config/MarvelApplicationConfig.java
@@ -1,14 +1,21 @@
 package katie.marvel.config;
 
+import co.aurasphere.jyandex.Jyandex;
 import katie.marvel.data.MarvelCharacterIDs;
 import katie.marvel.marvelApi.MarvelAPIConnector;
 import katie.marvel.util.Translator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:application.yml")
 public class MarvelApplicationConfig {
+
+    @Value("${translate.key}")
+    private String apiKey = "a";
 
     @Bean
     @Scope("singleton")
@@ -23,7 +30,12 @@ public class MarvelApplicationConfig {
 
 
     @Bean
-    Translator getTranslator() {
-        return new Translator();
+    Translator getTranslator(Jyandex jyandex) {
+        return new Translator(jyandex);
+    }
+
+    @Bean
+    Jyandex getJyandex() {
+        return new Jyandex(apiKey);
     }
 }

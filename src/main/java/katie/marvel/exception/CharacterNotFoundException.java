dcb4002f928498d85cc074168ab53e40be89697d
Katie Sanderson <katherina.sanderson@gmail.com>
2019-11-21 21:23:21 -0600

    Added response status 404

--- src/main/java/katie/marvel/exception/CharacterNotFoundException.java
+++ src/main/java/katie/marvel/exception/CharacterNotFoundException.java
@@ -1,5 +1,9 @@
 package katie.marvel.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CharacterNotFoundException extends RuntimeException {
 
     public CharacterNotFoundException(String exception) {
